a2670e69ff1213a2f52e7f5b6cf641a2ccfa3482
CodeBoy124 <koen7maart@gmail.com>
2025-10-07 21:30:06 +0200

    feat: error fallback system

--- groups.js
+++ groups.js
@@ -1,29 +1,35 @@
 let groups = {};
 
 function parseGroups() {
-  groups = {};
-  let selectedGroups = [];
-  let selectedGroupsPushMode = false;
-  const lines = inptGroups.value
-    .replace(/\r/g, "")
-    .split("\n")
-    .filter((line) => line.length != 0);
-  for (const line of lines) {
-    if (line[0] == ">") {
-      selectedGroupsPushMode = false;
-      for (let selectedGroup of selectedGroups) {
-        groups[selectedGroup].push(line.slice(1));
-      }
-    } else {
-      if (!groups.hasOwnProperty(line)) groups[line] = [];
-
-      if (selectedGroupsPushMode) {
-        selectedGroups.push(line);
+  const oldGroups = JSON.parse(JSON.stringify(groups));
+  try {
+    groups = {};
+    let selectedGroups = [];
+    let selectedGroupsPushMode = false;
+    const lines = inptGroups.value
+      .replace(/\r/g, "")
+      .split("\n")
+      .filter((line) => line.length != 0);
+    for (const line of lines) {
+      if (line[0] == ">") {
+        selectedGroupsPushMode = false;
+        for (let selectedGroup of selectedGroups) {
+          groups[selectedGroup].push(line.slice(1));
+        }
       } else {
-        selectedGroups = [line];
-      }
+        if (!groups.hasOwnProperty(line)) groups[line] = [];
 
-      selectedGroupsPushMode = true;
+        if (selectedGroupsPushMode) {
+          selectedGroups.push(line);
+        } else {
+          selectedGroups = [line];
+        }
+
+        selectedGroupsPushMode = true;
+      }
     }
+  } catch (error) {
+    groups = oldGroups;
+    console.error(error);
   }
 }
